be6db5fadc53d3a4fd399541b817ec62e89b468b
Drew Seeholzer <drews21f7@gmail.com>
2019-06-19 11:29:31 -0600

    Finished models

---
+++ PlaylistCoreData27/Models/Playlist+Convenience.swift
@@ -0,0 +1,18 @@
+//
+//  Playlist+Convenience.swift
+//  PlaylistCoreData27
+//
+//  Created by Drew Seeholzer on 6/19/19.
+//  Copyright © 2019 Drew Seeholzer. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+extension Playlist {
+    convenience init(name: String, context: NSManagedObjectContext = CoreDataStack.managedObjectContext) {
+        self.init(context: context)
+        self.name = name
+    }
+}
---
+++ PlaylistCoreData27/Models/Song+Convenience.swift
@@ -0,0 +1,19 @@
+//
+//  Song+Convenience.swift
+//  PlaylistCoreData27
+//
+//  Created by Drew Seeholzer on 6/19/19.
+//  Copyright © 2019 Drew Seeholzer. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Song {
+    convenience init(name: String, artist: String, playlist: Playlist, context: NSManagedObjectContext = CoreDataStack.managedObjectContext) {
+        self.init(context: context)
+        self.title = title
+        self.artist = artist
+        self.playlist = playlist
+    }
+}
